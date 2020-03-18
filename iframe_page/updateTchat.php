122513971699afa71c61079fb92981998ae2cf07
lostsh <0pdlna3@gmail.com>
2020-03-18 21:39:01 +0100

    the sha2 will come soon

--- iframe_page/updateTchat.php
+++ iframe_page/updateTchat.php
@@ -6,7 +6,7 @@
     }
     include("conn.php");
     $dataReq = $dataBase->query('SELECT nom_salle_msg, pseudo, date_post, txt_msg, DATE_FORMAT(date_post, \'[%d/%m/%Y] - [%H:%i]\') AS date_post_fr FROM msg WHERE nom_salle_msg LIKE "'.$_SESSION['salleTchat'].'" ORDER BY date_post ASC LIMIT 0, 10');
-
+    
     while($dataRecu = $dataReq->fetch() ){
         echo '<p>'.'<span class="pseudo">'.htmlspecialchars($dataRecu['pseudo']).'</span> : '.htmlspecialchars($dataRecu['txt_msg']).'<span class="datation">'.htmlspecialchars($dataRecu['date_post_fr']).'</span>'.'</p>';
     }
