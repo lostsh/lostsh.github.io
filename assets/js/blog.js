8a92cc4e14d4cda9c2d1cf4dd0f232a056f03b57
lostsh <0pdlna3@gmail.com>
2020-08-29 15:34:07 +0200

    update blog.js now integrate resume of the article

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,19 +1,28 @@
+/**
+ * This main function use an index file, and then generate a article for eatch blog post in the index file, in reverse order
+ **/
 function main(){
   var blogPath = "../blog/";
   var posts = readFile(blogPath+"postsIndex.txt").split('\n');
 
   var str = "";
+  var nbPosts = posts.length-1;
   for(var i in posts){
-    if(!posts[i]==""){
-      var articleBlogPath = blogPath+posts[i];
+    var currentPost = nbPosts-i;
+    if(!posts[currentPost]==""){
+      var articleBlogPath = blogPath+posts[currentPost];
       str+="Post : "+articleBlogPath+"<br>\n";
-      articleGenerator(articleBlogPath, getTitle(articleBlogPath), "28/08/20", "Quick description");
+      articleGenerator(articleBlogPath, getTitle(articleBlogPath), getDateFromPath(posts[currentPost]), getSummary(posts[currentPost]));
     }
   }
   console.log(str);
-  //document.write(str);
 }
 
+/**
+ * Read file return his content
+ * @apram file path
+ * @return row content of the file
+ */
 function readFile(fille){
   var baseFile = new XMLHttpRequest();
   var content;
@@ -25,6 +34,22 @@ function readFile(fille){
   return content;
 }
 
+/**
+ * Generate the HTML article and add it into the node with id="articleContainer"
+ * 
+ * Generate an article flowing this template :
+ * <a href="../blog/YYYY-MM-DD/article-title.html">
+ *  <article>
+ *    <h3>Article title</h3>
+ *    <time datetime="2020-08-19">2020-08-19</time>
+ *    <p>Quick description</p>
+ *  </article>
+ * </a>
+ * @param {*} link Path to the article.html file (used for href)
+ * @param {*} title Title of the article
+ * @param {*} dateTime Publication date of the article
+ * @param {*} description Sumary of the article content
+ */
 function articleGenerator(link, title, dateTime, description){
   var a = document.createElement("a");
   a.href = link;
@@ -46,9 +71,49 @@ function articleGenerator(link, title, dateTime, description){
   mainElement.appendChild(a);
 }
 
+/**
+ * Get the title of an article reading the article.html file
+ * @param {*} fille path of the article file
+ * @return text between <h3></h3> tag
+ */
 function getTitle(fille){
   var article = readFile(fille);
   var indexOpentH3 = article.indexOf("<h3>");
   var indexCloseH3 = article.indexOf("</h3>");
   return article.substring(indexOpentH3+4, indexCloseH3);
+}
+
+/**
+ * Get the date of an article with the name of the folder
+ * @param {*} strPath path to the article.html file
+ * @return date of the article YYYY-MM-DD
+ */
+function getDateFromPath(strPath){
+  return strPath.substring(0, strPath.indexOf("/"));
+}
+
+function getSummary(fille){
+  var summary = "";
+  var article = readFile(fille);
+  var indexOfFisrtPTagOpen = article.indexOf("<p>");
+  var indexOfFirstPTagClose = article.indexOf("</p>");
+  var firstPTagContent = article.substring(indexOfFisrtPTagOpen+3, indexOfFirstPTagClose);
+
+  var nbOfWorld = firstPTagContent.split(" ").length;
+  summary = firstPTagContent;
+  if(nbOfWorld > 7 && nbOfWorld < 25){
+    summary = summary.substring(0, indexOfStr(summary, " ", 13));
+    nbOfWorld = summary.split(" ").length;
+  }else if(nbOfWorld >= 25){
+    summary = summary.substring(0, indexOfStr(summary, " ", 20));
+  }
+  return summary;
+}
+
+function indexOfStr(str, sertchedStr, nbOfRepetion){
+  var index = 0;
+  for(var i=0;i<nbOfRepetion;i++){
+    index = str.indexOf(sertchedStr, index+1);
+  }
+  return index;
 }
