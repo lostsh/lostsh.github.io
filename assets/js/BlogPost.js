ad048a4da5416ff94de3ad1d80377bcf56b5e376
lostsh <0pdlna3@gmail.com>
2021-07-03 23:51:29 +0200

    blog engine start working

--- assets/js/BlogPost.js
+++ assets/js/BlogPost.js
@@ -1,2 +1,42 @@
 class BlogPost{
+    /**
+     * Blog ticket
+     * @param {*} title article title
+     * @param {*} url url of the article ticket
+     * @param {*} dt_post datetime post
+     * @param {*} dt_update datetime update
+     * @param {*} desc description
+     * @param {*} thumbnail url of the thumbnail picture
+     */
+    constructor(title, url, dt_post, dt_update, desc, thumbnail){
+        this.title = title;
+        this.url = url;
+        this.datetime_post = dt_post;
+        this.datetime_update = dt_update;
+        this.description = desc;
+        this.thumbnail = thumbnail;
+    }
+    
+    /**
+     * 
+     * @param {*} anchor DOM element of the <div>
+     */
+    show(anchor){
+        const viewerDom = anchor.children;
+        if(viewerDom.length != 4) return 1;
+        //set the div clickable
+        const url = this.url;//smol ugly thick to use anonymous func with event listener of a scoped var
+        anchor.addEventListener('click', function(){window.location = url});
+        //Title
+        viewerDom[0].textContent = this.title;
+        //datime
+        viewerDom[1].textContent = this.datetime_post + "_" + this.datetime_update;
+        //Picture
+        viewerDom[2].src = this.thumbnail;
+        viewerDom[2].alt = this.title;
+        //Description
+        viewerDom[3].textContent = this.description;
+        //everythig goes right
+        return 0;
+    }
 }
--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,67 @@
 function main(nbRequiredPosts) {
     var baseUrl = "../../blog/";
+    var postContainerAnchor = document.querySelector('#blogPostContainer');
+
+    var posts = getBlogPosts(baseUrl+'postsIndex.json');
+    posts.forEach(function(p){
+        var anchor = createHtmlPost(postContainerAnchor);
+        console.log(p.url);
+        p.show(anchor);
+    });
+}
+
+function createHtmlPost(baseAnchor){
+    const blogPostDiv = document.createElement('div');
+    blogPostDiv.className = "blogpost";
+    var h2 = document.createElement("h2");
+    blogPostDiv.appendChild(h2);
+    var span = document.createElement("span");
+    blogPostDiv.appendChild(span);
+    var img = document.createElement("img");
+    blogPostDiv.appendChild(img);
+    var p = document.createElement("p");
+    blogPostDiv.appendChild(p);
+
+    //add div to the blog post container
+    baseAnchor.appendChild(blogPostDiv);
+    return blogPostDiv;
+}
+
+/**
+ * Get an array of all blog posts of the file.
+ * @param {*} JsonFile 
+ * @returns Array of BlogPost
+ */
+function getBlogPosts(JsonFile){
+    const JsonData = JSON.parse(reader(JsonFile));
+    const posts = Array();
+    JsonData.articles.forEach( article => 
+        posts.push(
+            new BlogPost(
+                article.title, 
+                article.url,
+                article.datetime_post,
+                article.datetime_update,
+                article.description,
+                article.thumbnail
+            )
+        )
+    );
+    return posts;
+}
+
+/**
+ * Read file return his content
+ * @apram file path
+ * @return row content of the file
+ */
+ function reader(fileUrl) {
+    var request = new XMLHttpRequest();
+    var content;
+    request.open("GET", fileUrl, false);
+    request.send(null);
+    if (request.readyState === 4) {
+        content = request.responseText;
+    }
+    return content;
 }
